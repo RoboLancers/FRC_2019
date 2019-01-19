627e7e138917e3228aa2ae8a856f6bc35291f493
RoboLancers <7134858@philasd.org>
2019-01-19 15:05:43 -0500

    Created Cargo, Climber, and Hatch pistons in RobotMap

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -10,8 +10,30 @@ public class RobotMap {
     public static final int LEFT_SLAVE_2 = 5;
 
 
-    //Hatch
+    //HatchPivot
     public static final int PIVOT_MOTOR = 8;
+
+    //Hatch Pistons
+    public static final int HATCH1_FORWARD = 10;
+    public static final int HATCH1_REVERSE = 11;
+
+    public static final int HATCH2_FORWARD = 11;
+    public static final int HATCH2_REVERSE = 12;
+
+    public static final int HATCH3_FORWARD = 13;
+    public static final int HATCH3_REVERSE = 14 ;
+
+    //LiftOff
+    public static final int LIFTOFF_FORWARD = 15;
+    public static final int LIFTOFF_REVERSE = 16;
+
+    //Cargo
+    public static final int CARGO_BLOCK_FORWARD = 17;
+    public static final int CARGO_BLOCK_REVERSE = 18;
+
+    public static final int CARGO_PIVOT_FORWARD = 19;
+    public static final int CARGO_PIVOT_REVERSE = 20;
+
 
     //Climber
     public static final int CLIMBER_MOTOR = 9;
