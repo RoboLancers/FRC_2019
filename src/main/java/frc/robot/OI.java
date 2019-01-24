4991a543d7a71c08f39538a67e35f03c8287a7e7
Tomdotexe <nothomasdo@gmail.com>
2019-01-24 17:24:13 -0500

    Assigned Elevate command to button

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -2,13 +2,16 @@ package frc.robot;
 
 
 import com.robolancers.lib.wrappers.hid.XboxController;
+import frc.robot.commands.Elevate;
 import frc.robot.commands.UseCargoBlock;
 import frc.robot.commands.UseCargoPivot;
 import frc.robot.enums.CargoBlockState;
 import frc.robot.enums.CargoPivotState;
+import frc.robot.enums.LiftoffState;
 
 public class OI {
     public static XboxController xboxController = new XboxController(0)
             .toggleWhenPressed(XboxController.Button.START, new UseCargoPivot(CargoPivotState.UP))
-            .toggleWhenPressed(XboxController.Button.SELECT, new UseCargoBlock(CargoBlockState.UP));
+            .toggleWhenPressed(XboxController.Button.SELECT, new UseCargoBlock(CargoBlockState.UP))
+            .toggleWhenPressed(XboxController.Button.RIGHT_JOYSTICK_BUTTON, new Elevate(LiftoffState.DOWN));
 }
