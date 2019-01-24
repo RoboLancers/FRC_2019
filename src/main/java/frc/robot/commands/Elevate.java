a05bb15304c18066ea8f4b7d242b05d422af2785
Tomdotexe <nothomasdo@gmail.com>
2019-01-24 17:23:24 -0500

    Create command to use climber and liftoff

--- src/main/java/frc/robot/commands/Elevate.java
+++ src/main/java/frc/robot/commands/Elevate.java
@@ -3,19 +3,30 @@ package frc.robot.commands;
 import com.robolancers.lib.wrappers.hid.XboxController;
 import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.OI;
-import frc.robot.enums.ClimberState;
+import frc.robot.enums.LiftoffState;
 import frc.robot.subsystems.manipulators.Climber;
+import frc.robot.subsystems.manipulators.Liftoff;
 
-public class UseClimber extends Command {
+public class Elevate extends Command {
 
-    public UseClimber() {
+    private LiftoffState liftoffState;
+
+    public Elevate(LiftoffState liftoffState) {
         requires(Climber.getInstance());
+        requires(Liftoff.getInstance());
+        this.liftoffState = liftoffState;
 
     }
 
     protected void initialize(){
         Climber.getInstance().stop();
+        if(liftoffState == LiftoffState.UP){
+            Liftoff.getInstance().takeOff();
+        }else{
+            Liftoff.getInstance().land();
+        }
     }
+
     protected void execute(){
         if(OI.xboxController.getButtonState(XboxController.Button.X)){
             Climber.getInstance().setClimberMotorUp(1);
