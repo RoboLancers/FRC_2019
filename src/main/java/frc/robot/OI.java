22f7ce9f02e39e9b07c487bf3f893c21354812e6
Tomdotexe <nothomasdo@gmail.com>
2019-01-24 16:55:12 -0500

    Assigned buttons to Cargo commands

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -2,7 +2,13 @@ package frc.robot;
 
 
 import com.robolancers.lib.wrappers.hid.XboxController;
+import frc.robot.commands.UseCargoBlock;
+import frc.robot.commands.UseCargoPivot;
+import frc.robot.enums.CargoBlockState;
+import frc.robot.enums.CargoPivotState;
 
 public class OI {
-    public static XboxController xboxController = new XboxController(0);
+    public static XboxController xboxController = new XboxController(0)
+            .toggleWhenPressed(XboxController.Button.START, new UseCargoPivot(CargoPivotState.UP))
+            .toggleWhenPressed(XboxController.Button.SELECT, new UseCargoBlock(CargoBlockState.UP));
 }
