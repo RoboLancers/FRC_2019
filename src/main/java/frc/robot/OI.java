b5a8aadaf5c0fc49de3966fe118c29349c0ca3d9
Navin George <navin.george10@gmail.com>
2019-01-25 17:37:08 -0500

    Assigned buttons to hatch pivot and hatch

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -2,19 +2,14 @@ package frc.robot;
 
 
 import com.robolancers.lib.wrappers.hid.XboxController;
-import frc.robot.commands.Elevate;
-import frc.robot.commands.UseCargoBlock;
-import frc.robot.commands.UseCargoPivot;
-import frc.robot.commands.UseHatch;
-import frc.robot.enums.CargoBlockState;
-import frc.robot.enums.CargoPivotState;
-import frc.robot.enums.HatchState;
-import frc.robot.enums.LiftoffState;
+import frc.robot.commands.*;
+import frc.robot.enums.*;
 
 public class OI {
     public static XboxController xboxController = new XboxController(0)
             .toggleWhenPressed(XboxController.Button.START, new UseCargoPivot(CargoPivotState.UP))
             .toggleWhenPressed(XboxController.Button.SELECT, new UseCargoBlock(CargoBlockState.UP))
             .toggleWhenPressed(XboxController.Button.RIGHT_JOYSTICK_BUTTON, new Elevate(LiftoffState.DOWN))
-            .toggleWhenPressed(XboxController.Button.A, new UseHatch(HatchState.IN));
+            .toggleWhenPressed(XboxController.Button.A, new UseHatch(HatchState.IN))
+            .toggleWhenPressed(XboxController.Button.X, new UseHatchPivot(HatchPivotState.DOWN));
 }
