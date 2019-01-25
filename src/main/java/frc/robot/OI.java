c398c2df11824f08d8f07eec699435746ba0861a
Tomdotexe <nothomasdo@gmail.com>
2019-01-25 17:45:08 -0500

    Updated Elevate command to include climber enum

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -9,7 +9,7 @@ public class OI {
     public static XboxController xboxController = new XboxController(0)
             .toggleWhenPressed(XboxController.Button.START, new UseCargoPivot(CargoPivotState.UP))
             .toggleWhenPressed(XboxController.Button.SELECT, new UseCargoBlock(CargoBlockState.UP))
-            .toggleWhenPressed(XboxController.Button.RIGHT_JOYSTICK_BUTTON, new Elevate(LiftoffState.DOWN))
+            .toggleWhenPressed(XboxController.Button.RIGHT_JOYSTICK_BUTTON, new Elevate(LiftoffState.UP, ClimberState.UP))
             .toggleWhenPressed(XboxController.Button.A, new UseHatch(HatchState.IN))
             .toggleWhenPressed(XboxController.Button.X, new UseHatchPivot(HatchPivotState.DOWN));
 }
