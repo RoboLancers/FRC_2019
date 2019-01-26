b02221332b57714693faa435527aaf577b08d78e
Jenny & Ivan <wjenny003@yahoo.com>
2019-01-26 12:33:25 -0500

    Add motors into the array

--- src/main/java/frc/robot/subsystems/drivetrain/Transmission.java
+++ src/main/java/frc/robot/subsystems/drivetrain/Transmission.java
@@ -5,14 +5,12 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 import frc.robot.Constants;
 import org.ghrobotics.lib.mathematics.units.Length;
 import org.ghrobotics.lib.wrappers.ctre.FalconSRX;
-
+import java.util.Arrays;
 import java.util.List;
 
 public class Transmission extends Subsystem {
-
     private FalconSRX<Length> master, slave1, slave2;
-    private List<FalconSRX> allFalcons;
-
+    private List<FalconSRX<Length>> allFalcons;
 
     Transmission(int masterPort, int slave1Port, int slave2Port) {
         master = new FalconSRX<>(masterPort, Constants.nativeUnitModel, Constants.TIMEOUT_MS);
@@ -21,6 +19,8 @@ public class Transmission extends Subsystem {
 
         slave1.follow(master);
         slave2.follow(master);
+
+        allFalcons = Arrays.asList(master, slave1, slave2);
 
         for(FalconSRX<Length> falconSRX : allFalcons) {
             falconSRX.setKP(Constants.kDrivetrainKP);
