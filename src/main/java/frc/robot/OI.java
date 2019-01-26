633dde4e5b25be8fc15bc9c48a3cc145c5a8c7ef
RoboLancers <ivanzou6996@gmail.com>
2019-01-26 11:51:05 -0500

    Fixed OI clashes - Phuong

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -5,20 +5,20 @@ import frc.robot.commands.subsystems.manipulator.cargo.UseCargoBlock;
 import frc.robot.commands.subsystems.manipulator.cargo.UseCargoPivot;
 import frc.robot.commands.subsystems.manipulator.climber.UseClimberArm;
 import frc.robot.commands.subsystems.manipulator.climber.UseLiftoffPiston;
-import frc.robot.commands.subsystems.manipulator.hatch.UseHatch;
+import frc.robot.commands.subsystems.manipulator.hatch.UseHatchPiston;
 import frc.robot.commands.subsystems.manipulator.hatch.UseHatchPivot;
 import frc.robot.enums.cargo.CargoBlockState;
 import frc.robot.enums.cargo.CargoPivotState;
 import frc.robot.enums.climber.ClimberState;
 import frc.robot.enums.climber.LiftoffState;
+import frc.robot.enums.hatch.HatchPistonState;
 import frc.robot.enums.hatch.HatchPivotState;
-import frc.robot.enums.hatch.HatchState;
 
 public class OI {
     public static XboxController xboxController = new XboxController(0)
             .toggleWhenPressed(XboxController.Button.START, new UseCargoPivot(CargoPivotState.UP))
             .toggleWhenPressed(XboxController.Button.SELECT, new UseCargoBlock(CargoBlockState.UP))
-            .toggleWhenPressed(XboxController.Button.A, new UseHatch(HatchState.IN))
+            .toggleWhenPressed(XboxController.Button.A, new UseHatchPiston(HatchPistonState.IN))
             .toggleWhenPressed(XboxController.Button.X, new UseHatchPivot(HatchPivotState.DOWN))
             .toggleWhenPressed(XboxController.Button.LEFT_JOYSTICK_BUTTON, new UseLiftoffPiston(LiftoffState.UP))
             .toggleWhenPressed(XboxController.Button.RIGHT_JOYSTICK_BUTTON, new UseClimberArm(ClimberState.UP));
