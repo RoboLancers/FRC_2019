e258e39829d8e6b9d7aead9a19737694415650ce
Navin George <navin.george10@gmail.com>
2019-01-26 11:37:37 -0500

    Create flight controller in OI

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -1,19 +1,23 @@
 package frc.robot;
 
 
+import com.robolancers.lib.wrappers.hid.FlightController;
+import com.robolancers.lib.wrappers.hid.FlightController;
 import com.robolancers.lib.wrappers.hid.XboxController;
 import frc.robot.commands.subsystems.manipulator.cargo.ToggleCargoPivot;
 import frc.robot.commands.subsystems.manipulator.cargo.UseCargoBlock;
-import frc.robot.commands.subsystems.manipulator.hatch.UseHatch;
+import frc.robot.commands.subsystems.manipulator.cargo.UseCargoPivot;
+import frc.robot.commands.subsystems.manipulator.hatch.UseHatchPiston;
 import frc.robot.commands.subsystems.manipulator.hatch.UseHatchPivot;
 import frc.robot.enums.cargo.CargoBlockState;
 import frc.robot.enums.hatch.HatchPivotState;
-import frc.robot.enums.hatch.HatchState;
+import frc.robot.enums.hatch.HatchPistonState;
 
 public class OI {
     public static XboxController xboxController = new XboxController(0)
             .whenPressed(XboxController.Button.START, new ToggleCargoPivot())
             .whenPressed(XboxController.Button.SELECT, new UseCargoBlock(CargoBlockState.UP))
-            .whenPressed(XboxController.Button.A, new UseHatch(HatchState.IN))
+            .whenPressed(XboxController.Button.A, new UseHatchPiston(HatchPistonState.IN))
             .whenPressed(XboxController.Button.X, new UseHatchPivot(HatchPivotState.DOWN));
+    public static FlightController flightController = new FlightController(1);
 }
