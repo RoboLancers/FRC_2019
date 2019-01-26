9352962482b2674e2a982399f7c49550637c2d9c
Navin George <navin.george10@gmail.com>
2019-01-26 11:30:15 -0500

    Renamed to HatchPistonState and added double solenoid to enum class

---
+++ src/main/java/frc/robot/enums/hatch/HatchPistonState.java
@@ -0,0 +1,18 @@
+package frc.robot.enums.hatch;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+
+public enum HatchPistonState {
+    IN(DoubleSolenoid.Value.kForward), OUT(DoubleSolenoid.Value.kReverse);
+
+    DoubleSolenoid.Value value;
+    HatchPistonState(DoubleSolenoid.Value value) {
+        this.value = value;
+    }
+
+    public DoubleSolenoid.Value getValue() {
+        return value;
+    }
+
+
+}
