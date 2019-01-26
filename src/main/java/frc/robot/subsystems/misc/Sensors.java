484510aeb1535266c61d8c9db1781c51e648270c
RoboLancers <ivanzou6996@gmail.com>
2019-01-25 19:14:55 -0500

    Create NavX sensor

--- src/main/java/frc/robot/subsystems/misc/Sensors.java
+++ src/main/java/frc/robot/subsystems/misc/Sensors.java
@@ -1,8 +1,36 @@
 package frc.robot.subsystems.misc;
 
+import com.kauailabs.navx.frc.AHRS;
+import edu.wpi.first.wpilibj.SerialPort;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class Sensors extends Subsystem {
+
+    private AHRS navX;
+    public static Sensors instance;
+
+    Sensors(){
+        navX = new AHRS(SerialPort.Port.kUSB);
+    }
+
+    public double getAngle(){
+        return -navX.getAngle();
+    }
+
+    public double getHeading(){
+       return navX.getFusedHeading();
+    }
+
+    public void resetNavX(){
+        navX.reset();
+    }
+
+    public synchronized static Sensors getInstance(){
+        if(instance == null){
+            instance = new Sensors();
+        }
+        return instance;
+    }
 
 
     @Override
