4d367e86c733eb6053be0b76ee97f7786cde80f7
Navin George <navin.george10@gmail.com>
2019-01-29 17:28:59 -0500

    Make a more simple setter.

---
+++ src/main/java/frc/robot/commands/autonomous/commands/AutoHatchRelease.java
@@ -0,0 +1,21 @@
+package frc.robot.commands.autonomous.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.enums.hatch.HatchEjectorState;
+import frc.robot.subsystems.manipulators.hatch.HatchEjector;
+
+public class AutoHatchRelease extends Command {
+    public AutoHatchRelease() {
+        requires(HatchEjector.getInstance());
+    }
+
+    @Override
+    protected void execute() {
+            HatchEjector.getInstance().set(HatchEjectorState.OUT);
+    }
+
+    @Override
+    protected boolean isFinished() {
+        return !HatchEjector.getInstance().getLimitSwitch();
+    }
+}
--- src/main/java/frc/robot/commands/subsystems/manipulator/climber/UseClimberArm.java
+++ src/main/java/frc/robot/commands/subsystems/manipulator/climber/UseClimberArm.java
@@ -1,10 +1,11 @@
 package frc.robot.commands.subsystems.manipulator.climber;
 
+import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.InstantCommand;
 import frc.robot.enums.climber.ClimberState;
 import frc.robot.subsystems.manipulators.climber.ClimberArm;
 
-public class UseClimberArm extends InstantCommand {
+public class UseClimberArm extends Command {
     private ClimberState climberState;
 
     public UseClimberArm(ClimberState climberState) {
@@ -14,10 +15,11 @@ public class UseClimberArm extends InstantCommand {
 
     @Override
     public void initialize() {
-        if (climberState == ClimberState.UP) {
-            ClimberArm.getInstance().set(ClimberState.UP);
-        } else {
-            ClimberArm.getInstance().set(ClimberState.DOWN);
-        }
+        ClimberArm.getInstance().set(climberState);
+    }
+
+    @Override
+    protected boolean isFinished() {
+        return ClimberArm.getInstance().getPosition() == climberState.getPosition();
     }
 }
