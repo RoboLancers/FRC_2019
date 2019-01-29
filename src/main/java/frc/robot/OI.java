f3e4633f1b345566bb2795781bb193829b0cc657
Navin George <navin.george10@gmail.com>
2019-01-29 17:30:01 -0500

    Add controls

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -2,8 +2,23 @@ package frc.robot;
 
 import com.robolancers.lib.wrappers.hid.FlightController;
 import com.robolancers.lib.wrappers.hid.XboxController;
+import frc.robot.commands.subsystems.manipulator.cargo.UseCargoBlock;
+import frc.robot.commands.subsystems.manipulator.climber.AutoClimb;
+import frc.robot.commands.subsystems.manipulator.hatch.UseHatchEjector;
+import frc.robot.commands.subsystems.manipulator.hatch.UseHatchPivot;
+import frc.robot.enums.cargo.CargoBlockState;
+import frc.robot.enums.hatch.HatchEjectorState;
+import frc.robot.enums.hatch.HatchPivotState;
 
 public class OI {
-    public static XboxController xboxController = new XboxController(0);
-    public static FlightController flightController = new FlightController(1);
+    public static XboxController xboxController = new XboxController(0)
+            .whenPressed(XboxController.Trigger.LEFT_TRIGGER_BUTTON, new UseHatchPivot(HatchPivotState.FLOOR))
+            .whenPressed(XboxController.Trigger.RIGHT_TRIGGER_BUTTON, new UseHatchPivot(HatchPivotState.DEFENSE))
+            .whenPressed(XboxController.Button.LEFT_BUMPER, new UseHatchPivot(HatchPivotState.SCORING))
+            .whenPressed(XboxController.Button.RIGHT_BUMPER, new UseHatchEjector(HatchEjectorState.OUT))
+            .whenPressed(XboxController.Button.START, new AutoClimb());
+
+    public static FlightController flightController = new FlightController(1)
+            .whenPressed(FlightController.Button.TRIGGER, new UseCargoBlock(CargoBlockState.UNBLOCK))
+            .whenReleased(FlightController.Button.TRIGGER, new UseCargoBlock(CargoBlockState.BLOCK));
 }
