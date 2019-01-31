a1cea6e4960de16b4a88ebedb8f8d0d24f7e9084
Brian Chen <brianchen0810@hotmail.com>
2019-01-31 17:47:06 -0500

    Remove comments

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -19,18 +19,8 @@ import frc.robot.subsystems.manipulators.hatch.HatchEjector;
 import frc.robot.subsystems.manipulators.hatch.HatchPivot;
 import frc.robot.subsystems.misc.Sensors;
 
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the TimedRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the build.gradle file in the
- * project.
- */
 public class Robot extends TimedRobot {
-    /**
-     * This function is run when the robot is first started up and should be used
-     * for any initialization code.
-     */
+
     @Override
     public void robotInit() {
         Drivetrain.getInstance();
