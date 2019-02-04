2c285a3288b05da402469c70133161b5ea817682
Jenny & Ivan <wjenny003@yahoo.com>
2019-02-04 15:36:14 -0500

    Add encoder and angles to dashboard

--- src/main/java/frc/robot/NetworkInterface.java
+++ src/main/java/frc/robot/NetworkInterface.java
@@ -3,26 +3,33 @@ package frc.robot;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import frc.robot.subsystems.drivetrain.Drivetrain;
+import frc.robot.subsystems.manipulators.climber.ClimberArm;
+import frc.robot.subsystems.misc.Sensors;
 
 @SuppressWarnings({"FieldCanBeLocal", "WeakerAccess"})
 public class NetworkInterface {
     private static NetworkInterface instance;
 
     private ShuffleboardTab mainShuffleboardDisplay;
-    private NetworkTableEntry leftEncoderCountEntry, rightEncoderCountEntry;
+    private NetworkTableEntry leftEncoderCountEntry, rightEncoderCountEntry, armEncoderEntry, armAngleEntry, robotAngleEntry;
 
     private NetworkInterface(){
         mainShuffleboardDisplay = Shuffleboard.getTab("Main Display");
 
         leftEncoderCountEntry = mainShuffleboardDisplay.add("Left Encoder Count", 0.0).getEntry();
         rightEncoderCountEntry = mainShuffleboardDisplay.add("Right Encoder Count", 0.0).getEntry();
+        armEncoderEntry = mainShuffleboardDisplay.add("Arm Encoder Count", 0.0).getEntry();
+        armAngleEntry = mainShuffleboardDisplay.add("Arm Angle", 0.0).getEntry();
+        robotAngleEntry = mainShuffleboardDisplay.add("Robot Angle", 0.0).getEntry();
     }
 
     public void updateShuffleboard(){
         leftEncoderCountEntry.setDouble(Drivetrain.getInstance().getLeftTransmission().getMaster().getSensorPosition().getFeet());
         rightEncoderCountEntry.setDouble(Drivetrain.getInstance().getRightTransmission().getMaster().getSensorPosition().getFeet());
+        armEncoderEntry.setDouble(ClimberArm.getInstance().getPosition());
+        armAngleEntry.setDouble(ClimberArm.getInstance().getAngle());
+        robotAngleEntry.setDouble(Sensors.getInstance().getAngle());
     }
 
     public static synchronized NetworkInterface getInstance() {
