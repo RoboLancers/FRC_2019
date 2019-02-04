a57a4cedb2d60ad72e3691b40cab4bec9d510617
Jenny & Ivan <wjenny003@yahoo.com>
2019-02-04 15:36:28 -0500

    Make camera

--- src/main/java/frc/robot/subsystems/misc/Camera.java
+++ src/main/java/frc/robot/subsystems/misc/Camera.java
@@ -1,4 +1,31 @@
 package frc.robot.subsystems.misc;
 
+import com.robolancers.lib.wrappers.vision.JeVois;
+import edu.wpi.first.wpilibj.SerialPort;
+
 public class Camera {
+    private static Camera instance;
+
+    private JeVois frontJeVois, backJeVois;
+
+    private Camera(){
+        frontJeVois = new JeVois(true, SerialPort.Port.kUSB1);
+        backJeVois = new JeVois(true, SerialPort.Port.kUSB2);
+    }
+
+    public static synchronized Camera getInstance() {
+        if (instance == null) {
+            instance = new Camera();
+        }
+
+        return instance;
+    }
+
+    public JeVois getFrontJeVois() {
+        return frontJeVois;
+    }
+
+    public JeVois getBackJeVois() {
+        return backJeVois;
+    }
 }
