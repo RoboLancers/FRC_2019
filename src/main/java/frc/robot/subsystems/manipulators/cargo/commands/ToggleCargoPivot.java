f7b91a6cccae2a6d73070704d07b23fb4c93455e
Brian Chen <brianchen0810@hotmail.com>
2019-02-13 21:57:33 -0500

    Allow toggling cargo pivot

--- src/main/java/frc/robot/subsystems/manipulators/cargo/commands/ToggleCargoPivot.java
+++ src/main/java/frc/robot/subsystems/manipulators/cargo/commands/ToggleCargoPivot.java
@@ -1,26 +1,16 @@
 package frc.robot.subsystems.manipulators.cargo.commands;
 
-import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.InstantCommand;
 import frc.robot.subsystems.manipulators.cargo.CargoPivot;
 import frc.robot.subsystems.manipulators.cargo.enums.CargoPivotState;
 
-public class ToggleCargoPivot extends Command {
-
+public class ToggleCargoPivot extends InstantCommand {
     public ToggleCargoPivot() {
         requires(CargoPivot.getInstance());
     }
 
     @Override
-    protected void execute(){
-        if(CargoPivot.getInstance().get().getValue().equals(CargoPivotState.UP)){
-            CargoPivot.getInstance().set(CargoPivotState.DOWN);
-        }else {
-            CargoPivot.getInstance().set(CargoPivotState.UP);
-        }
-    }
-
-    @Override
-    protected boolean isFinished() {
-        return false;
+    protected void initialize(){
+        CargoPivot.getInstance().set(CargoPivot.getInstance().get() == CargoPivotState.UP ? CargoPivotState.DOWN : CargoPivotState.UP);
     }
 }
