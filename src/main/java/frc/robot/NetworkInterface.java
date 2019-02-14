efa5f1c46cde7854b7e4475440b4593cdd1491c9
Brian Chen <brianchen0810@hotmail.com>
2019-02-13 22:01:38 -0500

    Add starting position chooser in Shuffleboard

--- src/main/java/frc/robot/NetworkInterface.java
+++ src/main/java/frc/robot/NetworkInterface.java
@@ -1,42 +1,54 @@
 package frc.robot;
 
 import edu.wpi.first.networktables.NetworkTableEntry;
-import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.shuffleboard.*;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import frc.robot.autonomous.enums.StartingPosition;
+import frc.robot.autonomous.routines.LevelOneFrontCargoLeftStation;
 import frc.robot.subsystems.drivetrain.Drivetrain;
 import frc.robot.subsystems.manipulators.cargo.CargoBlock;
 import frc.robot.subsystems.manipulators.cargo.CargoPivot;
 import frc.robot.subsystems.manipulators.climber.ClimberArm;
 import frc.robot.subsystems.manipulators.climber.LiftoffPiston;
 import frc.robot.subsystems.manipulators.hatch.HatchEjector;
-import frc.robot.subsystems.manipulators.hatch.HatchPivot;
 import org.ghrobotics.lib.mathematics.units.derivedunits.VelocityKt;
 
 @SuppressWarnings({"FieldCanBeLocal", "WeakerAccess"})
 public class NetworkInterface {
     private static NetworkInterface instance;
 
-    private ShuffleboardTab mainShuffleboardDisplay;
+    private ShuffleboardTab debugDisplay, mainDisplay;
 
     private NetworkTableEntry
             leftEncoderCountEntry, rightEncoderCountEntry, leftVelocityEntry, rightVelocityEntry,
             armEncoderEntry, armAngleEntry, armErrorEntry, liftOffPistonStateEntry, armLimitSwitchVoltage,
             cargoBlockStateEntry, cargoPivotStateEntry,
-            hatchPivotEncoderCountEntry, hatchEjectorStateEntry, hatchLimitSwitchEntry,
+            hatchEjectorStateEntry, hatchLimitSwitchEntry,
             robotXEntry, robotYEntry, robotHeadingEntry;
 
     private ShuffleboardLayout drivetrainList, cargoList, climberList, hatchList, localizationList;
 
+    private SendableChooser<StartingPosition> startingPositionChooser;
+
     private NetworkInterface(){
-        mainShuffleboardDisplay = Shuffleboard.getTab("Main Display");
+        mainDisplay = Shuffleboard.getTab("Main Display");
 
-        drivetrainList = mainShuffleboardDisplay.getLayout("Drivetrain", BuiltInLayouts.kList).withPosition(0, 0).withSize(2, 5);
-        cargoList = mainShuffleboardDisplay.getLayout("Cargo", BuiltInLayouts.kList).withPosition(2, 0).withSize(2, 3);
+        startingPositionChooser = new SendableChooser<>();
 
-        climberList = mainShuffleboardDisplay.getLayout("Climber", BuiltInLayouts.kList).withPosition(4, 0).withSize(2, 5);
-        hatchList = mainShuffleboardDisplay.getLayout("Hatch", BuiltInLayouts.kList).withPosition(6, 0).withSize(2, 3);
+        for(StartingPosition startingPosition : StartingPosition.values()){
+            startingPositionChooser.addOption(startingPosition.name(), startingPosition);
+        }
 
-        localizationList = mainShuffleboardDisplay.getLayout("Localization", BuiltInLayouts.kList).withPosition(8, 0).withSize(2, 4);
+        debugDisplay = Shuffleboard.getTab("Debug Display");
+
+        drivetrainList = debugDisplay.getLayout("Drivetrain", BuiltInLayouts.kList).withPosition(0, 0).withSize(2, 5);
+        cargoList = debugDisplay.getLayout("Cargo", BuiltInLayouts.kList).withPosition(2, 0).withSize(2, 3);
+
+        climberList = debugDisplay.getLayout("Climber", BuiltInLayouts.kList).withPosition(4, 0).withSize(2, 5);
+        hatchList = debugDisplay.getLayout("Hatch", BuiltInLayouts.kList).withPosition(6, 0).withSize(2, 3);
+
+        localizationList = debugDisplay.getLayout("Localization", BuiltInLayouts.kList).withPosition(8, 0).withSize(2, 4);
 
         leftEncoderCountEntry = drivetrainList.add("Left Encoder Count", 0.0).getEntry();
         rightEncoderCountEntry = drivetrainList.add("Right Encoder Count", 0.0).getEntry();
@@ -55,21 +67,12 @@ public class NetworkInterface {
 
         liftOffPistonStateEntry = climberList.add("Liftoff Piston State", "").getEntry();
 
-        hatchPivotEncoderCountEntry = hatchList.add("Hatch Pivot Encoder Count", 0.0).getEntry();
         hatchEjectorStateEntry = hatchList.add("Hatch Ejector State", "").getEntry();
         hatchLimitSwitchEntry = hatchList.add("Hatch Limit Switch", false).getEntry();
 
         robotXEntry = localizationList.add("Robot X", 0.0).getEntry();
         robotYEntry = localizationList.add("Robot Y", 0.0).getEntry();
         robotHeadingEntry = localizationList.add("Robot Angle", 0.0).getEntry();
-
-//        NetworkTableInstance.getDefault()
-//                .getEntry("/CameraPublisher/FrontJeVois/streams")
-//                .setStringArray(new String[]{"mjpeg:http://roborio-321-frc.local:1180/?action=stream"});
-//
-//        NetworkTableInstance.getDefault()
-//                .getEntry("/CameraPublisher/BackJeVois/streams")
-//                .setStringArray(new String[]{"mjpeg:http://roborio-321-frc.local:1181/?action=stream"});
 
         Shuffleboard.startRecording();
     }
@@ -81,9 +84,6 @@ public class NetworkInterface {
         leftVelocityEntry.setDouble(VelocityKt.getFeetPerSecond(Drivetrain.getInstance().getLeftMotor().getVelocity()));
         rightVelocityEntry.setDouble(VelocityKt.getFeetPerSecond(Drivetrain.getInstance().getRightMotor().getVelocity()));
 
-//        leftVelocityEntry.setDouble(Drivetrain.getInstance().getLeftTransmission().getMaster().getSelectedSensorVelocity());
-//        rightVelocityEntry.setDouble(Drivetrain.getInstance().getRightTransmission().getMaster().getSelectedSensorVelocity());
-
         cargoBlockStateEntry.setString(CargoBlock.getInstance().get().toString());
         cargoPivotStateEntry.setValue(CargoPivot.getInstance().get().toString());
 
@@ -94,7 +94,7 @@ public class NetworkInterface {
 
         liftOffPistonStateEntry.setString(LiftoffPiston.getInstance().get().toString());
 
-        hatchPivotEncoderCountEntry.setDouble(HatchPivot.getInstance().getMaster().getSelectedSensorPosition());
+        hatchEjectorStateEntry.setString(HatchEjector.getInstance().get().toString());
         hatchLimitSwitchEntry.setBoolean(HatchEjector.getInstance().hasHatch());
 
         robotXEntry.setDouble(Drivetrain.getInstance().getLocalization().getRobotPosition().getTranslation().getX().getFeet());
