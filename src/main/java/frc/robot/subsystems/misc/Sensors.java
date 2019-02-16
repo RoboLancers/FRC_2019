3f42dca84a8275acbf6e6223014e6fbdc79c3724
Brian Chen <brianchen0810@hotmail.com>
2019-02-16 11:29:56 -0500

    Change back to Navx

--- src/main/java/frc/robot/subsystems/misc/Sensors.java
+++ src/main/java/frc/robot/subsystems/misc/Sensors.java
@@ -1,30 +1,16 @@
 package frc.robot.subsystems.misc;
 
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-import com.ctre.phoenix.sensors.PigeonIMU;
-import com.ctre.phoenix.sensors.PigeonIMU_StatusFrame;
 import com.kauailabs.navx.frc.AHRS;
 import edu.wpi.first.wpilibj.SerialPort;
-import edu.wpi.first.wpilibj.command.Subsystem;
-import frc.robot.RobotMap;
 
 @SuppressWarnings("WeakerAccess")
 public class Sensors{
-    private PigeonIMU pigeonIMU;
-    private double[] yawPitchRoll;
+    private AHRS gyro;
 
     private static Sensors instance;
 
     private Sensors() {
-        TalonSRX pigeonTalon = new TalonSRX(RobotMap.MISC.PIGEON_TALON);
-        pigeonIMU = new PigeonIMU(pigeonTalon);
-
-        pigeonTalon.configFactoryDefault();
-        pigeonIMU.setStatusFramePeriod(PigeonIMU_StatusFrame.CondStatus_9_SixDeg_YPR, 10);
-
-        calibratePigeon();
-
-        yawPitchRoll = new double[3];
+        gyro = new AHRS(SerialPort.Port.kMXP);
     }
 
     public synchronized static Sensors getInstance() {
@@ -34,20 +20,19 @@ public class Sensors{
         return instance;
     }
 
-    public double getAngle() {
-        return pigeonIMU.getFusedHeading();
+    public synchronized double getAngle() {
+        return gyro.getAngle();
     }
 
     public double getPitch() {
-        pigeonIMU.getYawPitchRoll(yawPitchRoll);
-        return yawPitchRoll[1];
+        return gyro.getPitch();
+    }
+
+    public double getRoll() {
+        return gyro.getRoll();
     }
 
     public void resetHeading() {
-        pigeonIMU.setFusedHeading(0);
-    }
-
-    public void calibratePigeon(){
-        pigeonIMU.enterCalibrationMode(PigeonIMU.CalibrationMode.Temperature);
+        gyro.reset();
     }
 }
