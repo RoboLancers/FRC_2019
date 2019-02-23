5dea8c680945875b6ab6aea87412ba53f1c749d5
Brian Chen <brianchen0810@hotmail.com>
2019-02-22 22:26:37 -0500

    Disallow auto hatch releasing when robot is browned out and strobe leds to red

--- src/main/java/frc/robot/subsystems/manipulators/hatch/commands/AutoHatchRelease.java
+++ src/main/java/frc/robot/subsystems/manipulators/hatch/commands/AutoHatchRelease.java
@@ -1,9 +1,12 @@
 package frc.robot.subsystems.manipulators.hatch.commands;
 
+import com.robolancers.lib.wrappers.Blinkin;
+import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import edu.wpi.first.wpilibj.command.WaitCommand;
 import frc.robot.subsystems.manipulators.hatch.enums.HatchEjectorState;
 import frc.robot.subsystems.manipulators.hatch.enums.HatchHolderState;
+import frc.robot.subsystems.misc.commands.UseLED;
 
 public class AutoHatchRelease extends CommandGroup {
     public AutoHatchRelease() {
@@ -12,5 +15,13 @@ public class AutoHatchRelease extends CommandGroup {
         addSequential(new UseHatchEjector(HatchEjectorState.EJECT));
         addSequential(new WaitCommand(0.25));
         addSequential(new UseHatchEjector(HatchEjectorState.RETRACT));
+    }
+
+    @Override
+    protected void initialize(){
+        if(RobotController.isBrownedOut()){
+            cancel();
+            new UseLED(Blinkin.PatternType.STROBE_RED);
+        }
     }
 }
