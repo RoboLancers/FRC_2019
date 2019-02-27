a47cc8dc974e0bb9b125f96a95a45ae320c1f80b
Tomdotexe <nothomasdo@gmail.com>
2019-02-27 16:50:31 -0500

    Made so motors can run before touching level 3

--- src/main/java/frc/robot/subsystems/manipulators/climber/commands/UseAutoClimb.java
+++ src/main/java/frc/robot/subsystems/manipulators/climber/commands/UseAutoClimb.java
@@ -1,20 +1,16 @@
 package frc.robot.subsystems.manipulators.climber.commands;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
-import frc.robot.Constants;
-import frc.robot.subsystems.drivetrain.commands.UseDrivetrain;
+import frc.robot.subsystems.drivetrain.commands.UseClimbDrive;
 import frc.robot.subsystems.manipulators.climber.enums.ClimberState;
 import frc.robot.subsystems.manipulators.climber.enums.LiftoffState;
-import frc.robot.subsystems.misc.Sensors;
 
 public class UseAutoClimb extends CommandGroup {
     public UseAutoClimb() {
         addSequential(new UseClimberArm(ClimberState.DOWN));
         addSequential(new UseLiftoffPiston(LiftoffState.DOWN));
-        if (Sensors.getInstance().getPitch() >= Constants.CLIMBER.CLIMBING_ANGLE) {
-            addSequential(new UseDrivetrain());
-            addParallel(new UseClimberArm(ClimberState.UP));
-        }
+        addParallel(new UseClimbDrive());
         addSequential(new UseLiftoffPiston(LiftoffState.UP));
+        addSequential(new UseClimberArm(ClimberState.UP));
     }
 }
