96d9630afc45a8a84a332dd92869b9928c5259a3
Tomdotexe <nothomasdo@gmail.com>
2019-02-28 15:22:09 -0500

    Deleted pointless if statement

--- src/main/java/frc/robot/subsystems/drivetrain/commands/UseClimbDrive.java
+++ src/main/java/frc/robot/subsystems/drivetrain/commands/UseClimbDrive.java
@@ -6,18 +6,14 @@ import frc.robot.subsystems.drivetrain.Drivetrain;
 import frc.robot.subsystems.misc.Sensors;
 
 public class UseClimbDrive extends Command {
-    //then drive once within margin
-
     public UseClimbDrive(){
         requires(Drivetrain.getInstance());
     }
 
     @Override
     protected void execute(){
-        if(Sensors.getInstance().getPitch() >= Constants.CLIMBER.CLIMBING_ANGLE){
-            Drivetrain.getInstance().getLeftMotor().setPercentOutput(1);
-            Drivetrain.getInstance().getRightMotor().setPercentOutput(1);
-        }
+        Drivetrain.getInstance().getLeftMotor().setPercentOutput(1);
+        Drivetrain.getInstance().getRightMotor().setPercentOutput(1);
     }
 
     @Override
